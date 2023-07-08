c1776f348ba5ba0ae5dbb2943577224e5f3f3a6f
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-08 22:50:27 +0530

    Create Lecture2-CipherSchools.py

---
+++ Lecture2-CipherSchools.py
@@ -0,0 +1,9 @@
+# Difference between Python 3 and Python 2
+# What is python3?
+# Why Python3?
+# Example
+# Output formatting
+name=input("Enter your name")
+print(f"Your name is : {name}")
+
+# What is python2?
