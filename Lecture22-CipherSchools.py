f82e33349df83a18de525a2d8006e5d7bd2c0393
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 21:57:14 +0530

    Update Lecture22-CipherSchools.py

--- Lecture22-CipherSchools.py
+++ Lecture22-CipherSchools.py
@@ -1,3 +1,22 @@
 # int() function
 # input() function always takes input as string format
 # so we have to convert it into integer if we needed
+
+number_one=int(intput("Enter first number : "))
+number_two=int(intput("Enter second number : "))
+total_number = number_one + number_two
+print("Total no is "+ total_number)
+
+# str(4) 
+#       4 ---> "4"
+
+# int(4) 
+#      "4" ---> 4
+
+# float(4)
+#      "4" ---> 4.0
+
+number1 = str(5)
+number2 = float("44")
+number3 = int("33")
+print(number2 + number3)
