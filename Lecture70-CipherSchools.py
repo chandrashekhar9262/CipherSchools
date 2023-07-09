eda28ecabd7c2f483ec264302bf4bfee9de19b45
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 13:19:11 +0530

    Create Lecture70-CipherSchools.py

---
+++ Lecture70-CipherSchools.py
@@ -0,0 +1,64 @@
+# step argument in range function in python
+# 1 step
+for i in range(1,11):
+  print(i)
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# 2 step (even number)
+for i in range(0,11,2):
+  print(i)
+# 0 
+# 2 
+# 4 
+# 6 
+# 8 
+# 10
+
+# 2 step (odd number)
+for i in range(1,11,2):
+  print(i)
+# 1
+# 3
+# 5
+# 7
+# 9
+
+
+# reverse
+for i in range(10,-1,-1):
+  print(i)
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+# 0
+
+
+# negative
+for i in range(-1,-11,-1):
+  print(i)
+# -1
+# -2
+# -3
+# -4
+# -5
+# -6
+# -7
+# -8
+# -9
+# -10
