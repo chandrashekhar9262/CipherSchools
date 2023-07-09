4664ccf6f7aa8ef751034ed4e81d50e4958a6838
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 18:39:53 +0530

    Create Lecture21-CipherSchools.py

---
+++ Lecture21-CipherSchools.py
@@ -0,0 +1,10 @@
+# User input in Python
+# input() function
+
+name = input("Enter your name : ")
+print("Hello, " + name)
+
+age= input("Enter your age : ")
+print("Hello, " + name + ", You are " + age + "years old")
+
+# input() function always takes input as String format only
