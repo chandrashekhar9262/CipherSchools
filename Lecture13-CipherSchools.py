6c7880a4a6a5c7a82a1746b706aa481411757e46
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 12:46:48 +0530

    Create Lecture13-CipherSchools.py

---
+++ Lecture13-CipherSchools.py
@@ -0,0 +1,11 @@
+# Exercise 1:
+# *********** Print these lines ************
+# this is \\ double backslash
+# this is /\/\/\/\/\ mountain
+# he is    awesome (use escape sequence instead of manual spaces)
+# \" \n \t \' (Print these as output)
+
+print("this is \\\\ double backslash")
+print("these are /\\/\\/\\/\\/\\/\\ mountain")
+print("he is\tawesome")
+print(" \\\" \\n \\t \\\'")
