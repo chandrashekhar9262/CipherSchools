eee581bc931258c67ff3332428840472e3d8c64e
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 12:17:48 +0530

    Update Lecture10-CipherSchools.py

--- Lecture10-CipherSchools.py
+++ Lecture10-CipherSchools.py
@@ -2,3 +2,8 @@
 #print("Hello "World" world")
 
 print("Hello \"World\" World")
+print("Line A \n Line B \n Line C")
+print("My name \t Chandra Shekhar Kumar")
+print("This is \ Backslash")
+print("This is double Backslash \\\\")
+print("Hell\blo")
