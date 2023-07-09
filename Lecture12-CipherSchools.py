d170146b3f1442b8a3222731a240184d0be30ea9
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 12:32:13 +0530

    Create Lecture12-CipherSchools.py

---
+++ Lecture12-CipherSchools.py
@@ -0,0 +1,12 @@
+# Escape sequence as normal text
+# Output : Line A \n Line B
+print("Line A \\n Line B")
+
+# Output : Line A \t Line B
+print("Line A \\t Line B")
+
+# Output : This is Double backslash \\
+print("This is Double backslash \\\\")
+
+# Output : \" \' 
+print(" \\\" \\\'")
