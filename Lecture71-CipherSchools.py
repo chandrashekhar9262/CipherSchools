21e862e9ec1a637ef6f1e5b922cc470aa9feb0c0
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 22:15:31 +0530

    Create Lecture71-CipherSchools.py

---
+++ Lecture71-CipherSchools.py
@@ -0,0 +1,61 @@
+# For loop and String in Python
+
+
+name = "Chandra Shekhar"
+for i in range(len(name)):
+  print(name[i])
+# C
+# h
+# a
+# n
+# d
+# r
+# a
+
+# S
+# h
+# e
+# k
+# h
+# a
+# r
+
+
+name = "Chandra Shekhar"
+for i in name:
+  print(i)
+# C
+# h
+# a
+# n
+# d
+# r
+# a
+
+# S
+# h
+# e
+# k
+# h
+# a
+# r
+
+
+for i in "Rohit":
+  print(i)
+# R
+# o
+# h
+# i
+# t
+
+
+#1256 ---> 1+2+5+6
+num = input("enter a number :")
+total = 0
+for i in num:
+  total += int(i)
+print(total)
+
+# Ex: input: 1256
+#     output: 14
