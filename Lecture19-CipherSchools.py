be00299a42f1444ece183fd7f902ededdec87f23
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 18:21:35 +0530

    Update Lecture19-CipherSchools.py

--- Lecture19-CipherSchools.py
+++ Lecture19-CipherSchools.py
@@ -6,3 +6,27 @@ print(r"Line A \n Line B")
 # print emoji
 print("\U0001F601")
 print("\U0001F602")
+
+
+# Python as calculator
+print(2+4)
+print(3**3)
+print(2**0.5)
+print(round(7**0.5),2)
+
+
+# precedence rule
+# Parenthesis ()
+# Exponent ^
+# *,/,//,%
+# +,-
+# 
+
+# Variables in Python
+name="Chandra Shekhar Kumar"
+print(name)
+
+n=1234
+print(n)
+
+# etc.
