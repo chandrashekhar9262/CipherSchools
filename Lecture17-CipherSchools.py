4351c18daf5cb18a37f415aaca08f413eec43eae
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 18:00:15 +0530

    Create Lecture17-CipherSchools.py

---
+++ Lecture17-CipherSchools.py
@@ -0,0 +1,28 @@
+# Python as calculator
+print(2+3)
+
+print(5+2*4)
+
+print(2/4)
+
+print(4/2)               # floating point division
+
+print(4//2)              # integer division
+
+print(2//4)              # integer division
+
+print(2**3)              # 2^3
+
+print(2**0.5)            # root of 2
+
+print(round(2**0.5),2)   # rounded output 2 digit after decimal
+
+print(2**3**2)           # 2^9
+
+# Precedence rule:
+
+# 1. Parenthesis () ---> Highest
+# 2. Exponent ^     ---> Right to left
+# 3. *,/,//,%       ---> Left to right
+# 4. +,-            ---> Left to right
+
