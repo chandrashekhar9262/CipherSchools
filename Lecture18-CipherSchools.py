439fc065386895c629548f8a663c4aa719e71d41
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 18:07:11 +0530

    Create Lecture18-CipherSchools.py

---
+++ Lecture18-CipherSchools.py
@@ -0,0 +1,27 @@
+# Variables in Python
+
+# No data type is used with variable name in Python.
+# Only a name is required for variable initialization.
+# Example:
+n=2
+print(n)
+
+n=4
+print(n)
+
+name="Chandra Shekhar Kumar"
+print(name)
+
+name=123
+print(name)
+
+
+# Naming conventions
+
+# When we have to declare a variable with more than one letter or word then we must have to separate with (_) underscore.
+# Ex:
+
+user_one_name="Chandra Shekhar"
+print(user_one_name)
+
+#This is a snake case writing
