f531d1734aabf5bb117c54858fe89976fdfaa5a9
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 13:02:56 +0530

    Update Lecture16-CipherSchools.py

--- Lecture16-CipherSchools.py
+++ Lecture16-CipherSchools.py
@@ -1,1 +1,11 @@
 # Print emojis in Python
+# https://unicode.org/emoji/charts/full-emoji-list.html
+# if emoji unicode is : U+1F602
+# then replace + with 000 (+ ---> 000)
+#Example:
+print("\U0001F601")
+# 😁
+print("\U0001F603")
+# 😝
+print("\U0001F923")
+# 😄
