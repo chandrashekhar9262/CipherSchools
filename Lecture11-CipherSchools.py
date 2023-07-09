049bb4f350b649a1324199f5873dd16da890bdf3
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-09 12:24:22 +0530

    Create Lecture11-CipherSchools.py

---
+++ Lecture11-CipherSchools.py
@@ -0,0 +1,7 @@
+# Comments in Python
+# This is a comment
+# \n is used for new line
+# \\ is used as backslash
+# \\\\ is used for double backslash
+print("These are comments")
+print("Shortcut for comment in VS Code 'CTRL + /' ")
