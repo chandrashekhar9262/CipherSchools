e28733943c9f2afd26cd39acefa2d2b1a03bf468
Chandra Shekhar Kumar <93523466+chandrashekhar9262@users.noreply.github.com>
2023-07-10 21:52:33 +0530

    Create Lecture74-CipherSchools.py

---
+++ Lecture74-CipherSchools.py
@@ -0,0 +1,8 @@
+# print vs return in python
+# return is used to return a value return by function
+def add_three(a,b,c):
+  return a+b+c
+  #print(a+b+c)
+
+# add_three(5,5,5)
+print(add_three(5,5,5))
